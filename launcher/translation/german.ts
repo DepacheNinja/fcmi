5b44a97b8869ecdf2d36337b05d1ba2a86f1f042
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2026-03-14 19:20:34 +0100

    Translated using Weblate (German)
    Currently translated at 100.0% (925 of 925 strings)
    
    Translated using Weblate (German)
    
    Currently translated at 100.0% (1096 of 1096 strings)
    
    Translated using Weblate (German)
    
    Currently translated at 100.0% (414 of 414 strings)
    
    Co-authored-by: Laserlicht <13953785+Laserlicht@users.noreply.github.com>
    Translate-URL: https://hosted.weblate.org/projects/vcmi/client/de/
    Translate-URL: https://hosted.weblate.org/projects/vcmi/editor/de/
    Translate-URL: https://hosted.weblate.org/projects/vcmi/launcher/de/
    Translation: VCMI/Game client
    Translation: VCMI/Launcher
    Translation: VCMI/Map Editor

--- launcher/translation/german.ts
+++ launcher/translation/german.ts
@@ -738,7 +738,7 @@ Exklusiver Vollbildmodus - das Spiel nimmt den gesamten Bildschirm ein und verwe
     <message>
         <location filename="../settingsView/csettingsview_moc.ui" line="1201"/>
         <source>Nearest</source>
-        <translation>Nearest</translation>
+        <translation>Nächster</translation>
     </message>
     <message>
         <location filename="../settingsView/csettingsview_moc.ui" line="1206"/>
